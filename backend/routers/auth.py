df9d15c4d2e4890d051f5fa3f4b14a116af4cca5
Nehuen Lian <nehuenn.lian@gmail.com>
2025-10-16 11:32:38 -0300

    validate password and email in backend. (/login and /register)

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -7,6 +7,7 @@ from data_access import crud
 from data_access.database import get_db
 from security.tokens_utils import create_access_token
 from validations.schemas import UserLogin, UserRegister
+from validations.general_validations import validate_email, validate_password_conditions
 
 limiter = Limiter(key_func=get_remote_address)
 router = APIRouter()
@@ -14,6 +15,11 @@ router = APIRouter()
 @router.post("/auth/login")
 @limiter.limit("5/minute")
 def login(request: Request, user_data: UserLogin, db: Session=Depends(get_db)):
+
+    email_valid = validate_email(user_data.email)
+    if not email_valid: 
+        raise HTTPException(status_code=400, detail="Invalid email format.")
+
     user = crud.get_user_by_email(db, user_data.email)
     if not user:
         raise HTTPException(status_code=404, detail="User not found.")
@@ -29,6 +35,15 @@ def login(request: Request, user_data: UserLogin, db: Session=Depends(get_db)):
 @router.post("/auth/register")
 @limiter.limit("3/minute")
 def register(request: Request, user_data: UserRegister, db: Session=Depends(get_db)):
+
+    email_valid = validate_email(user_data.email)
+    if not email_valid: 
+        raise HTTPException(status_code=400, detail="Invalid email format.")
+    
+    password_valid = validate_password_conditions(user_data.password)
+    if not password_valid:
+        raise HTTPException(status_code=400, detail="Invalid password format.")
+
     user_exists = crud.get_user_by_email(db, user_data.email)
     if user_exists:
         raise HTTPException(status_code=409, detail="This email is already in use.")
