97762e43efd07a92a284f749e63a5232e664d24b
Nehuen Lian <nehuenn.lian@gmail.com>
2025-10-23 00:02:52 -0300

    add load to refresh if access token is expired when access protected routes

---
+++ frontend/src/routes/+layout.server.js
@@ -0,0 +1,37 @@
+import jwt_decode from 'jwt-decode';
+
+export async function load({ cookies, fetch }) {
+    const token = cookies.get('session');
+    let needRefresh = true;
+
+    if (token) {
+        try {
+            const decoded = jwt_decode(token);
+            const now = Math.floor(Date.now() / 1000);
+            if (decoded.exp - now > 120) {
+                needRefresh = false;
+            }
+        } catch {
+             needRefresh = true;
+        }
+    }
+
+    if (needRefresh) {
+        const res = await fetch('http://127.0.0.1:8000/api/auth/refresh', { 
+            method: 'POST', credentials: 'include' 
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            cookies.set('session', data.access_token, {
+                httpOnly: true,
+                path: '/',
+                maxAge: 60*60
+            });
+        } else {
+            cookies.delete('session', { path: '/' });
+        }
+    }
+
+    return {};
+}
