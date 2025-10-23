40a102dc216a583b20a45dc491e68c8a9164404d
Nehuen Lian <nehuenn.lian@gmail.com>
2025-10-22 21:18:36 -0300

    added create_refresh_token function

--- backend/security/tokens_utils.py
+++ backend/security/tokens_utils.py
@@ -6,11 +6,21 @@ from jose import jwt
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
 JWT_ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES"))
+JWT_REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_REFRESH_TOKEN_EXPIRE_MINUTES"))
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(UTC) + timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp" : expire})
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+
+    return encoded_jwt
+
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire =  datetime.now(UTC) + timedelta(minutes=JWT_REFRESH_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp" : expire, "type" : "refresh"})
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
     return encoded_jwt
