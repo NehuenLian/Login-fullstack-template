0af84f458a9ebb7dcbd2de93b5843fb6fa20d06a
Nehuen Lian <nehuenn.lian@gmail.com>
2025-12-13 01:40:21 -0300

    update create_db script for development db

--- backend/scripts/create_db.py
+++ backend/scripts/create_db.py
@@ -1,14 +1,23 @@
+import asyncio
 import os
+from sqlmodel import SQLModel, Field
+from sqlalchemy.ext.asyncio import create_async_engine
 
-from dotenv import load_dotenv
-from sqlmodel import Session, SQLModel, create_engine
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    email: str
+    password: str
+    role: str = "user"
 
-from data_access.models import User
+current_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(current_dir, "test.db")
+DATABASE_URL = f"sqlite+aiosqlite:///{db_path}"
 
-load_dotenv()
+engine = create_async_engine(DATABASE_URL, echo=True)
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-engine = create_engine(DATABASE_URL, echo=True)
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
 
-SQLModel.metadata.create_all(engine)
-print("DB created.")
+if __name__ == "__main__":
+    asyncio.run(init_db())
